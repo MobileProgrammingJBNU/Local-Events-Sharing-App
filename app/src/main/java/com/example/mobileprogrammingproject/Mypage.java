8bc79006a486ba8b5d4bf941d11f3c9ec171defe
DodoJBNU <dh723@jbnu.ac.kr>
2023-11-21 20:55:12 +0900

    Feat : 프로필 수정 버튼에 액티비티 전환 기능 추가

--- app/src/main/java/com/example/mobileprogrammingproject/Mypage.java
+++ app/src/main/java/com/example/mobileprogrammingproject/Mypage.java
@@ -4,19 +4,31 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.animation.ObjectAnimator;
 import android.animation.ValueAnimator;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 
 public class Mypage extends AppCompatActivity {
 
     ProgressBar progressBar;
-
+    Button changeButton;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mypage);
         progressBar = findViewById(R.id.progressBar);
         animateProgressBar(36); // 원하는 프로그래스 값으로 애니메이션 실행
+
+        changeButton = findViewById(R.id.profileChange_btn); // 프로필 수정 버튼
+
+        changeButton.setOnClickListener(new View.OnClickListener(){  // 프로필 수정 버튼 클릭 시, 프로필 변경 액티비티로 이동
+            public void onClick(View v){
+                Intent intent = new Intent(getApplicationContext(), ProfileChange.class);
+                startActivity(intent);
+            }
+        });
     }
     private void animateProgressBar(int targetProgress) {
         ValueAnimator animator = ValueAnimator.ofInt(0, targetProgress);
