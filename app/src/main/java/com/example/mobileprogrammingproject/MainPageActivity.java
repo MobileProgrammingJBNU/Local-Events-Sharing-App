07344ebe35bc64d765992b56c687c07f3bd2a3a8
KiserLine <hts000203@gmail.com>
2023-11-23 15:10:28 +0900

    Feat: 지도 UI 구현

---
+++ app/src/main/java/com/example/mobileprogrammingproject/MainPageActivity.java
@@ -0,0 +1,286 @@
+package com.example.mobileprogrammingproject;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
+import android.graphics.Color;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
+import android.os.Build.VERSION;
+import android.os.Bundle;
+import android.text.InputType;
+import android.util.Base64;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+import net.daum.mf.map.api.CameraUpdateFactory;
+import net.daum.mf.map.api.MapPOIItem;
+import net.daum.mf.map.api.MapPoint;
+import net.daum.mf.map.api.MapPointBounds;
+import net.daum.mf.map.api.MapPolyline;
+import net.daum.mf.map.api.MapView;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class MainPageActivity extends AppCompatActivity implements MapView.CurrentLocationEventListener, MapView.MapViewEventListener {
+    private MapView mapView;
+    private ViewGroup mapViewContainer;
+    private MapPOIItem marker;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_page);
+
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("키해시는 :", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+
+        // 권한ID를 가져옴
+        int permission = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.INTERNET);
+
+        int permission2 = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION);
+
+        int permission3 = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION);
+
+        // 권한이 열려있는지 확인
+        if (permission == PackageManager.PERMISSION_DENIED || permission2 == PackageManager.PERMISSION_DENIED || permission3 == PackageManager.PERMISSION_DENIED) {
+            // 마쉬멜로우 이상버전부터 권한을 물어본다
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                // 권한 체크(READ_PHONE_STATE의 requestCode를 1000으로 세팅
+                requestPermissions(
+                        new String[]{Manifest.permission.INTERNET, Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},
+                        1000);
+            }
+            return;
+        }
+
+        //지도
+        mapView = new MapView(this);
+        mapViewContainer = findViewById(R.id.map_view);
+        mapViewContainer.addView(mapView);
+        mapView.setMapViewEventListener(this);
+        mapView.setCurrentLocationTrackingMode(MapView.CurrentLocationTrackingMode.TrackingModeOnWithoutHeading);
+
+        // 기능 버튼 초기화
+        Button btnWritePost = findViewById(R.id.btnWritePost);
+        btnWritePost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // AlertDialog를 사용하여 게시물 내용을 입력받음
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainPageActivity.this);
+                builder.setTitle("게시물 작성");
+
+                // EditText를 추가하여 사용자로부터 입력받음
+                final EditText input = new EditText(MainPageActivity.this);
+                input.setInputType(InputType.TYPE_CLASS_TEXT);
+                builder.setView(input);
+
+                builder.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // 사용자가 입력한 내용을 가져옴
+                        String postContent = input.getText().toString();
+
+                        // 현재 지도 중심 좌표를 가져옴
+                        MapPoint centerMapPoint = mapView.getMapCenterPoint();
+
+                        // 마커를 생성하고 설정
+                        if (marker != null) {
+                            mapView.removePOIItem(marker); // 이미 생성된 마커가 있다면 제거
+                        }
+
+                        marker = new MapPOIItem();
+                        marker.setItemName(postContent);
+                        marker.setTag(1);
+                        marker.setMapPoint(centerMapPoint);
+                        marker.setMarkerType(MapPOIItem.MarkerType.CustomImage);
+                        marker.setCustomImageResourceId(R.drawable.marker);
+                        marker.setCustomImageAutoscale(false);
+                        marker.setCustomImageAnchor(0.5f, 1.0f);
+
+                        // 마커를 지도에 추가
+                        mapView.addPOIItem(marker);
+                    }
+                });
+
+                builder.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+
+                builder.show();
+            }
+        });
+
+        // 폴리라인
+        MapPolyline polyline = new MapPolyline();
+        polyline.setTag(1000);
+        polyline.setLineColor(Color.argb(128, 255, 51, 0)); // Polyline 컬러 지정.
+
+        // Polyline 좌표 지정.
+        polyline.addPoint(MapPoint.mapPointWithGeoCoord(37.479928, 126.900169));
+        polyline.addPoint(MapPoint.mapPointWithGeoCoord(37.480624, 126.900735));
+        polyline.addPoint(MapPoint.mapPointWithGeoCoord(37.481667, 126.900713));
+
+        // Polyline 지도에 올리기.
+        mapView.addPolyline(polyline);
+
+        // 지도뷰의 중심좌표와 줌레벨을 Polyline이 모두 나오도록 조정.
+        MapPointBounds mapPointBounds = new MapPointBounds(polyline.getMapPoints());
+        int padding = 100; // px
+        mapView.moveCamera(CameraUpdateFactory.newMapPointBounds(mapPointBounds, padding));
+    }
+
+    // 마커를 현재 위치에 추가하는 메서드
+    private void addMarkerAtCurrentLocation() {
+        if (mapView != null) {
+            // 현재 지도 중심 좌표를 가져옴
+            MapPoint centerMapPoint = mapView.getMapCenterPoint();
+
+            // 마커를 생성하고 설정
+            if (marker != null) {
+                mapView.removePOIItem(marker); // 이미 생성된 마커가 있다면 제거
+            }
+
+            marker = new MapPOIItem();
+            marker.setItemName("New Marker");
+            marker.setTag(1);
+            marker.setMapPoint(centerMapPoint);
+            marker.setMarkerType(MapPOIItem.MarkerType.CustomImage);
+            marker.setCustomImageResourceId(R.drawable.marker);
+            marker.setCustomImageAutoscale(false);
+            marker.setCustomImageAnchor(0.5f, 1.0f);
+
+            // 마커를 지도에 추가
+            mapView.addPOIItem(marker);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grandResults) {
+        // READ_PHONE_STATE의 권한 체크 결과를 불러옴
+        super.onRequestPermissionsResult(requestCode, permissions, grandResults);
+        if (requestCode == 1000) {
+            boolean check_result = true;
+
+            // 모든 퍼미션을 허용했는지 체크
+            for (int result : grandResults) {
+                if (result != PackageManager.PERMISSION_GRANTED) {
+                    check_result = false;
+                    break;
+                }
+            }
+
+            // 권한 체크에 동의를 하지 않으면 안드로이드 종료
+            if (!check_result) {
+                finish();
+            }
+        }
+    }
+
+    @Override
+    public void onCurrentLocationUpdate(MapView mapView, MapPoint mapPoint, float v) {
+
+    }
+
+    @Override
+    public void onCurrentLocationDeviceHeadingUpdate(MapView mapView, float v) {
+
+    }
+
+    @Override
+    public void onCurrentLocationUpdateFailed(MapView mapView) {
+
+    }
+
+    @Override
+    public void onCurrentLocationUpdateCancelled(MapView mapView) {
+
+    }
+
+    @Override
+    public void onMapViewInitialized(MapView mapView) {
+
+    }
+
+    @Override
+    public void onMapViewCenterPointMoved(MapView mapView, MapPoint mapPoint) {
+
+    }
+
+    @Override
+    public void onMapViewZoomLevelChanged(MapView mapView, int i) {
+
+    }
+
+    @Override
+    public void onMapViewSingleTapped(MapView mapView, MapPoint mapPoint) {
+        // 클릭한 위치의 위도, 경도 정보를 가져옴
+        double lat = mapPoint.getMapPointGeoCoord().latitude;
+        double lng = mapPoint.getMapPointGeoCoord().longitude;
+
+        // 마커 위치를 클릭한 위치로 옮김
+        if (marker != null) {
+            marker.setMapPoint(MapPoint.mapPointWithGeoCoord(lat, lng));
+        }
+
+        String message = "위도: " + lat + "경도: " + lng;
+        Log.d("클릭한 위치 정보", message);
+    }
+
+    @Override
+    public void onMapViewDoubleTapped(MapView mapView, MapPoint mapPoint) {
+
+    }
+
+    @Override
+    public void onMapViewLongPressed(MapView mapView, MapPoint mapPoint) {
+
+    }
+
+    @Override
+    public void onMapViewDragStarted(MapView mapView, MapPoint mapPoint) {
+
+    }
+
+    @Override
+    public void onMapViewDragEnded(MapView mapView, MapPoint mapPoint) {
+
+    }
+
+    @Override
+    public void onMapViewMoveFinished(MapView mapView, MapPoint mapPoint) {
+
+    }
+}
