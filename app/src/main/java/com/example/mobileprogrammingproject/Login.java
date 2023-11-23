b3a0eb890fb432846a4e986b40632602cb46d986
leedohyeon <132376178+dodojbnu@users.noreply.github.com>
2023-11-23 15:52:08 +0900

    Feat: 로그인, 회원가입 UI 제작

---
+++ app/src/main/java/com/example/mobileprogrammingproject/Login.java
@@ -0,0 +1,19 @@
+package com.example.mobileprogrammingproject;
+
+import android.annotation.SuppressLint;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.WindowInsets;
+
+import com.example.mobileprogrammingproject.databinding.ActivityLoginBinding;
+
+public class Login extends AppCompatActivity {
+ }
