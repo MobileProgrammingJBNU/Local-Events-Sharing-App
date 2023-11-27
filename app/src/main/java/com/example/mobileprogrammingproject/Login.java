e5a9fb5c48065521117ef81b74c4f79d186a7598
rladntlr <tlrrns08@gmail.com>
2023-11-27 19:16:17 +0900

    Add: 로그인, 회원가입 버튼별 작업 수행

--- app/src/main/java/com/example/mobileprogrammingproject/Login.java
+++ app/src/main/java/com/example/mobileprogrammingproject/Login.java
@@ -5,6 +5,7 @@ import android.annotation.SuppressLint;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
@@ -12,8 +13,22 @@ import android.os.Looper;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowInsets;
+import android.widget.TextView;
 
 import com.example.mobileprogrammingproject.databinding.ActivityLoginBinding;
 
 public class Login extends AppCompatActivity {
+     TextView sign;
+
+     protected void onCreate(Bundle savedInstanceState){
+           super.onCreate(savedInstanceState);
+           setContentView(R.layout.activity_login);
+            //회원가입 버튼
+           sign = findViewById(R.id.signin);
+            //회원가입 누르면 페이지 이동
+           sign.setOnClickListener(v -> {
+                Intent intent = new Intent(this, Signup.class);
+                startActivity(intent);
+           });
+     }
  }
