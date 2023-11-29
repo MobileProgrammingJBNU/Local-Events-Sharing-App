176339383f08bc3753323228e60b6e3f6a2625b1
KiserLine <hts000203@gmail.com>
2023-11-29 16:46:11 +0900

    Feat: 내용 입력 버튼 추가

--- app/src/main/java/com/example/mobileprogrammingproject/MainPageActivity.java
+++ app/src/main/java/com/example/mobileprogrammingproject/MainPageActivity.java
@@ -1,35 +1,27 @@
 package com.example.mobileprogrammingproject;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-
 import android.Manifest;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.Signature;
 import android.graphics.Color;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.os.Build;
-import android.os.Build.VERSION;
 import android.os.Bundle;
-import android.text.InputType;
 import android.util.Base64;
 import android.util.Log;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageButton;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -48,6 +40,7 @@ public class MainPageActivity extends AppCompatActivity implements MapView.Curre
     private ViewGroup mapViewContainer;
     private MapPOIItem marker;
     private BottomNavigationView bottomNavigationView;
+    private Button btnWritePost;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,21 +60,12 @@ public class MainPageActivity extends AppCompatActivity implements MapView.Curre
             e.printStackTrace();
         }
 
-        // 권한ID를 가져옴
-        int permission = ContextCompat.checkSelfPermission(this,
-                Manifest.permission.INTERNET);
+        int permission = ContextCompat.checkSelfPermission(this, Manifest.permission.INTERNET);
+        int permission2 = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
+        int permission3 = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION);
 
-        int permission2 = ContextCompat.checkSelfPermission(this,
-                Manifest.permission.ACCESS_FINE_LOCATION);
-
-        int permission3 = ContextCompat.checkSelfPermission(this,
-                Manifest.permission.ACCESS_COARSE_LOCATION);
-
-        // 권한이 열려있는지 확인
         if (permission == PackageManager.PERMISSION_DENIED || permission2 == PackageManager.PERMISSION_DENIED || permission3 == PackageManager.PERMISSION_DENIED) {
-            // 마쉬멜로우 이상버전부터 권한을 물어본다
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                // 권한 체크(READ_PHONE_STATE의 requestCode를 1000으로 세팅
                 requestPermissions(
                         new String[]{Manifest.permission.INTERNET, Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION},
                         1000);
@@ -89,49 +73,19 @@ public class MainPageActivity extends AppCompatActivity implements MapView.Curre
             return;
         }
 
-        //지도
         mapView = new MapView(this);
         mapViewContainer = findViewById(R.id.map_view);
         mapViewContainer.addView(mapView);
         mapView.setMapViewEventListener(this);
         mapView.setCurrentLocationTrackingMode(MapView.CurrentLocationTrackingMode.TrackingModeOnWithoutHeading);
-        /*
-        // 기능 버튼 초기화
-        ImageButton btnWritePost = findViewById(R.id.btnWritePost);
 
-        btnWritePost.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainPageActivity.this, WritingBoardActivity.class);
-                startActivity(intent);
-            }
-        });
-        // 마이페이지 이동 버튼
-        ImageButton mypage_btn = findViewById(R.id.btnMyPage);
-        mypage_btn.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
-                Intent intent = new Intent(MainPageActivity.this, Mypage.class);
-                startActivity(intent);
-            }
-        });
-
-        // Screen 액티비티로 이동하는 임시버튼
-        Button temp_btn = findViewById(R.id.temp);
-        temp_btn.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
-                Intent intent = new Intent(MainPageActivity.this, Screen.class);
-                startActivity(intent);
-            }
-        });
-         */
         bottomNavigationView = findViewById(R.id.bottomNavigationView);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 int itemId = item.getItemId();
                 if (itemId == R.id.tab_write) {
-                    Intent intent = new Intent(MainPageActivity.this, WritingBoardActivity.class);
-                    startActivity(intent);
+                    showLocationSelectionDialog();
                     return true;
                 } else if (itemId == R.id.tab_refresh) {
                     return true;
@@ -145,70 +99,61 @@ public class MainPageActivity extends AppCompatActivity implements MapView.Curre
             }
         });
 
-        // 폴리라인
+        btnWritePost = findViewById(R.id.btnWritePost);
+        btnWritePost.setVisibility(View.GONE);
+
         MapPolyline polyline = new MapPolyline();
         polyline.setTag(1000);
-        polyline.setLineColor(Color.argb(128, 255, 51, 0)); // Polyline 컬러 지정.
+        polyline.setLineColor(Color.argb(128, 255, 51, 0));
 
-        // Polyline 좌표 지정.
         polyline.addPoint(MapPoint.mapPointWithGeoCoord(37.479928, 126.900169));
         polyline.addPoint(MapPoint.mapPointWithGeoCoord(37.480624, 126.900735));
         polyline.addPoint(MapPoint.mapPointWithGeoCoord(37.481667, 126.900713));
 
-        // Polyline 지도에 올리기.
         mapView.addPolyline(polyline);
 
-        // 지도뷰의 중심좌표와 줌레벨을 Polyline이 모두 나오도록 조정.
         MapPointBounds mapPointBounds = new MapPointBounds(polyline.getMapPoints());
-        int padding = 100; // px
+        int padding = 100;
         mapView.moveCamera(CameraUpdateFactory.newMapPointBounds(mapPointBounds, padding));
-    }
 
-    // 마커를 현재 위치에 추가하는 메서드
-    private void addMarkerAtCurrentLocation() {
-        if (mapView != null) {
-            // 현재 지도 중심 좌표를 가져옴
-            MapPoint centerMapPoint = mapView.getMapCenterPoint();
-
-            // 마커를 생성하고 설정
-            if (marker != null) {
-                mapView.removePOIItem(marker); // 이미 생성된 마커가 있다면 제거
+        mapView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                mapView.setCurrentLocationTrackingMode(MapView.CurrentLocationTrackingMode.TrackingModeOff);
+                return false;
             }
-
-            marker = new MapPOIItem();
-            marker.setItemName("New Marker");
-            marker.setTag(1);
-            marker.setMapPoint(centerMapPoint);
-            marker.setMarkerType(MapPOIItem.MarkerType.CustomImage);
-            marker.setCustomImageResourceId(R.drawable.marker);
-            marker.setCustomImageAutoscale(false);
-            marker.setCustomImageAnchor(0.5f, 1.0f);
-
-            // 마커를 지도에 추가
-            mapView.addPOIItem(marker);
-        }
+        });
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grandResults) {
-        // READ_PHONE_STATE의 권한 체크 결과를 불러옴
-        super.onRequestPermissionsResult(requestCode, permissions, grandResults);
-        if (requestCode == 1000) {
-            boolean check_result = true;
+    private void showLocationSelectionDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("행사가 열리는 위치를 지정하시오");
 
-            // 모든 퍼미션을 허용했는지 체크
-            for (int result : grandResults) {
-                if (result != PackageManager.PERMISSION_GRANTED) {
-                    check_result = false;
-                    break;
+        builder.setPositiveButton("확인", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                MapPoint centerMapPoint = mapView.getMapCenterPoint();
+
+                if (marker != null) {
+                    mapView.removePOIItem(marker);
                 }
-            }
 
-            // 권한 체크에 동의를 하지 않으면 안드로이드 종료
-            if (!check_result) {
-                finish();
+                marker = new MapPOIItem();
+                marker.setItemName("Event Location");
+                marker.setTag(1);
+                marker.setMapPoint(centerMapPoint);
+                marker.setMarkerType(MapPOIItem.MarkerType.CustomImage);
+                marker.setCustomImageResourceId(R.drawable.marker);
+                marker.setCustomImageAutoscale(false);
+                marker.setCustomImageAnchor(0.5f, 1.0f);
+
+                mapView.addPOIItem(marker);
+
+                btnWritePost.setVisibility(View.VISIBLE);
             }
-        }
+        });
+
+        builder.create().show();
     }
 
     @Override
@@ -248,11 +193,9 @@ public class MainPageActivity extends AppCompatActivity implements MapView.Curre
 
     @Override
     public void onMapViewSingleTapped(MapView mapView, MapPoint mapPoint) {
-        // 클릭한 위치의 위도, 경도 정보를 가져옴
         double lat = mapPoint.getMapPointGeoCoord().latitude;
         double lng = mapPoint.getMapPointGeoCoord().longitude;
 
-        // 마커 위치를 클릭한 위치로 옮김
         if (marker != null) {
             marker.setMapPoint(MapPoint.mapPointWithGeoCoord(lat, lng));
         }
@@ -284,5 +227,18 @@ public class MainPageActivity extends AppCompatActivity implements MapView.Curre
     @Override
     public void onMapViewMoveFinished(MapView mapView, MapPoint mapPoint) {
 
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        btnWritePost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainPageActivity.this, WritingBoardActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
