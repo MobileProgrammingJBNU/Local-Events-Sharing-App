acb99b50b7351bd890bc16501abb0ec46cc73e8e
sinweol <wjddnwls728@gmail.com>
2023-12-09 18:26:02 +0900

    feat:db에 댓글 저장

--- app/src/main/java/com/example/mobileprogrammingproject/Screen.java
+++ app/src/main/java/com/example/mobileprogrammingproject/Screen.java
@@ -1,6 +1,5 @@
 package com.example.mobileprogrammingproject;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,25 +8,72 @@ import android.animation.ValueAnimator;
 import android.os.Bundle;
 import android.animation.ObjectAnimator;
 import android.animation.ValueAnimator;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ProgressBar;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import org.checkerframework.checker.units.qual.C;
 
 
 public class Screen extends AppCompatActivity {
     ProgressBar progressBar;
     Toolbar toolbar;
+
+    DatabaseReference databaseReference;
+    public static class Comment {
+        private String message;
+        private String userId;
+        private long timestamp;
+
+        // Default constructor required for calls to DataSnapshot.getValue(Comment.class)
+        public Comment() {
+        }
+
+        public Comment(String message, long timestamp, String userId) {
+            this.message = message;
+            this.timestamp = timestamp;
+            this.userId = userId;
+        }
+    }
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_screen);
         progressBar = findViewById(R.id.progressBar);
         animateProgressBar(36); // 원하는 프로그래스 값으로 애니메이션 실행
-
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true); // 툴바 왼쪽에, 뒤로가기 버튼 추가.
 
+
+        FirebaseDatabase firebaseDatabase=FirebaseDatabase.getInstance();
+        databaseReference-firebaseDatabase.getReference("comments");
+        Button send = findViewById(R.id.comment_btn_send);
+        EditText comment_text = findViewById(R.id.comment_edit_message);
+        send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String message = comment_text.getText().toString();
+                long timestamp = System.currentTimeMillis(); // 밀리초 단위 현재 시간
+                String userId = //bd에서 유저id가져오기
+
+                        saveCommentToFirebase(userId, timestamp, message);
+            }
+        });
     }
+    private void saveCommentToFirebase(String userId, long timestamp, String message) {
+        String commentId = databaseReference.push().getKey();
+        Comment comment = new Comment(userId, timestamp, message);
+        databaseReference.child(commentId).setValue(comment);
+    }
+
     private void animateProgressBar(int targetProgress) {
         ValueAnimator animator = ValueAnimator.ofInt(0, targetProgress);
         animator.setDuration(500); // 애니메이션 기간 (1초로 설정, 필요에 따라 조절)
@@ -42,6 +88,8 @@ public class Screen extends AppCompatActivity {
 
         animator.start();
     }
+
+}
     public void onBackPressed() {
         super.onBackPressed();
         // 여기에 원하는 동작을 추가하세요
